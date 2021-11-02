d026a33d5518d687b1698fd301d3bf30aeead671
Kerem <1741kerem@gmail.com>
2021-11-02 19:47:54 +0300

    Add files via upload

---
+++ WindowsFormsApp18/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace WindowsFormsApp18
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbSayi = new System.Windows.Forms.TextBox();
+            this.lblSayi = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(229, 420);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(247, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Faktöriyel Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbSayi
+            // 
+            this.tbSayi.Location = new System.Drawing.Point(264, 352);
+            this.tbSayi.Name = "tbSayi";
+            this.tbSayi.Size = new System.Drawing.Size(100, 20);
+            this.tbSayi.TabIndex = 2;
+            // 
+            // lblSayi
+            // 
+            this.lblSayi.AutoSize = true;
+            this.lblSayi.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSayi.Location = new System.Drawing.Point(291, 329);
+            this.lblSayi.Name = "lblSayi";
+            this.lblSayi.Size = new System.Drawing.Size(46, 20);
+            this.lblSayi.TabIndex = 3;
+            this.lblSayi.Text = "Sayı";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(247, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSayi);
+            this.Controls.Add(this.tbSayi);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbSayi;
+        private System.Windows.Forms.Label lblSayi;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp18/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApp18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int sayi = 0, faktoriyel = 1;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sayi = Convert.ToInt32(tbSayi.Text);
+            label1.Text = "";
+
+            if (sayi >= 1)
+            {
+                if (sayi == 0 || sayi == 1)
+                {
+                    tbSayi.Text = "1";
+                }
+
+                while (sayi >= 1)
+                {
+                    label1.Text = label1.Text + sayi + "*";
+                    faktoriyel = faktoriyel * sayi;
+                    sayi--;
+                    
+                }
+
+                label1.Text += ("=" + faktoriyel);
+                listBox1.Items.Clear();
+                listBox1.Items.Add(Convert.ToString(faktoriyel));
+            }
+            else
+            {
+                MessageBox.Show("Sayı 0'dan Küçük Olamaz!");
+            }
+        }
+    }
+}
